9afb414876afa4c965f50272bbd4efdc3729504e
Pranay Prakash <pranay.gp@gmail.com>
2017-09-25 01:15:10 -0500

    more

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,1 +1,39 @@
-console.log("Hello World!");
+const body = document.querySelector('body');
+const nav = document.querySelector('nav');
+const navLinks = nav.querySelectorAll('li');
+const sections = document.querySelectorAll('.section');
+
+const initialNavHeight = nav.clientHeight;
+const initialFontSize = parseInt(window.getComputedStyle(nav.querySelector('li'), null).getPropertyValue('font-size'));
+
+window.addEventListener('scroll', e => {
+  const scrollPos = body.scrollTop;  
+
+  // Nav Bar Resizing
+  const progress =  Math.min(scrollPos/(window.innerHeight * 0.5), 1.0);
+  nav.style.height = initialNavHeight - (0.5 * progress * initialNavHeight) + "px";
+  navLinks.forEach(node => {
+    node.style.lineHeight = nav.style.height;
+    node.style.fontSize = (initialFontSize - (0.15 * progress * initialFontSize)) + "px";
+  })
+
+  let sectionScrollPositions = Array.prototype.map.call(sections, s => s.offsetTop);
+
+  let currentSection = -1;
+  sectionScrollPositions.forEach((s, i) => {
+    if((s-parseInt(nav.style.height)) <= scrollPos) {
+      currentSection = i;
+    }
+  })
+
+  if(currentSection >= 0) {
+    navLinks.forEach((node, i) => {
+      node.classList.remove("selected");
+      if(i === currentSection) {
+        node.classList.add("selected");
+      }
+    })
+  }
+
+
+})
