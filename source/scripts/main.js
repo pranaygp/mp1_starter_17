d2f88bf0e42c384495812ef7d1fca28141140cda
Pranay Prakash <pranay.gp@gmail.com>
2017-09-25 02:32:59 -0500

    more!

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -2,7 +2,11 @@ const body = document.querySelector('body');
 const nav = document.querySelector('nav');
 const navLinks = nav.querySelectorAll('li');
 const sections = document.querySelectorAll('.section');
+const caroussel = document.querySelector('.caroussel');
+const carousselSlides = caroussel.querySelectorAll('li');
+const carousselControls = caroussel.querySelectorAll('.controls');
 
+let currentCarrouselSlide = 0;
 const initialNavHeight = nav.clientHeight;
 const initialFontSize = parseInt(window.getComputedStyle(nav.querySelector('li'), null).getPropertyValue('font-size'));
 
@@ -38,6 +42,36 @@ window.addEventListener('scroll', e => {
       }
     })
   }
-
-
 })
+
+function carousselClickHandler(e) {
+
+  switch(e.target.id) {
+    case 'next':
+      currentCarrouselSlide = (currentCarrouselSlide+1)%3;
+      break;
+    case 'prev':
+      currentCarrouselSlide = (currentCarrouselSlide-1)%3;
+      break;
+  }
+
+  switch(currentCarrouselSlide) {
+    case 0:
+    case -0:
+      carousselSlides[0].style.marginTop = "0px";
+      carousselSlides[1].style.marginTop = "0px";
+      break;
+    case 1:
+    case -1:
+      carousselSlides[0].style.marginTop = "-100%";
+      carousselSlides[1].style.marginTop = "0px";
+      break;
+    case 2:
+    case -2:
+      carousselSlides[0].style.marginTop = "-100%";
+      carousselSlides[1].style.marginTop = "-100%";
+      break;
+  }
+}
+
+carousselControls.forEach(el => el.addEventListener('click', carousselClickHandler))
